5048eac5d831e1cb9d0b444860decfcaf7f33f89
DeFoldik <topmod234@gmail.com>
2025-02-28 05:44:31 +0300

    Add DB

--- src/main/java/com/carsharing/carsharing/exception/GlobalExceptionHandler.java
+++ src/main/java/com/carsharing/carsharing/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.carsharing.carsharing.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CarNotFound.class)
     public ResponseEntity<String> handleCarNotFoundException(CarNotFound ex) {
